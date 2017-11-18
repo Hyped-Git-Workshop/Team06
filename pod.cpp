34aebc76ab20230ddf238fb6716e9e0142d7acc7
Srividhya Sireesh <svidhya@c040172.wlan.net.ed.ac.uk>
2017-11-18 15:11:30 +0000

    new Task 5

--- pod.cpp
+++ pod.cpp
@@ -25,10 +25,10 @@ double Pod::get_max_speed()
 /// TASKS 3 (PERSON A) & 4 (PERSON B) //////////////////////////////////////////
 double Pod::get_temperature()
 {
-  return 0;
+  return 28;
 }
 double Pod::get_pressure()
 {
-  return 0;
+  return 28;
 }
 /// END OF TASKS 3A & 4B ///////////////////////////////////////////////////////
